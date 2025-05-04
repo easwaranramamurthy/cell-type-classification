a84fca53291ad97e4ab11c86715b68389358cb42
Easwaran Ramamurthy <easwaranramamurthy@Easwarans-MacBook-Pro.local>
2025-05-04 13:54:31 -0400

    fix: making sure arch works

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -1,33 +1,42 @@
-from self_attention import SelfAttention
-from mlp_classifier import MLP
+from models.self_attention import SelfAttention
+from models.mlp_classifier import MLP
 import torch.nn as nn
 
 class Transformer(nn.Module):
     def __init__(self,
-                 num_layers=6,
+                 num_layers = 6,
+                 vocab_size = 20000,
                  num_tokens = 2048,
                  d_model=512,
                  d_q_k_v=128,
-                 num_heads=6):
-        super(SelfAttention, self).__init__()
-        self.embed = nn.Embedding(embedding_dim=d_model)
+                 num_heads=6,
+                 num_classes=4
+                 ):
+        super(Transformer, self).__init__()
+        self.embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
         self.attn_layers = [SelfAttention(num_tokens, d_model, d_q_k_v, num_heads) for _ in range(num_layers)]
         self.attn_layer_norms = [nn.LayerNorm(d_model) for _ in range(num_layers)]
         self.feed_fwd_layers = [nn.Linear(in_features=d_model,out_features=d_model) for _ in range(num_layers)]
         self.feed_fwd_activations = [nn.Sigmoid() for _ in range(num_layers)]
         self.feed_fwd_layer_norms = [nn.LayerNorm(d_model) for _ in range(num_layers)]
-        self.final_mlp = MLP()
-
-
+        self.final_mlp = MLP(num_classes=num_classes)
 
     def forward(self, input):
         embedding = self.embed(input)
+        print(f"Embedding {embedding.shape}")
         for attn_layer, attn_layer_norm, feed_fwd_layer, feed_fwd_activation, feed_fwd_layer_norm in zip(self.attn_layers, self.attn_layer_norms, self.feed_fwd_layers, self.feed_fwd_activations, self.feed_fwd_layer_norms):
             attn_output = attn_layer(embedding)
+            print(f"Attention output {attn_output.shape}")
             residual_added = embedding+attn_output
+            print(f"Residual added to attention output: {residual_added.shape}")
             normed_embedding = attn_layer_norm(residual_added)
+            print(f"Layer normalized embedding {normed_embedding.shape}")
             feed_fwd_output = feed_fwd_activation(feed_fwd_layer(normed_embedding))
+            print(f"Feed forward output {feed_fwd_output.shape}")
             residual_added_feed_fwd_output = normed_embedding+feed_fwd_output
+            print(f"Residual added to feed forward {residual_added_feed_fwd_output.shape}")
             embedding = feed_fwd_layer_norm(residual_added_feed_fwd_output)
+            print(f"Final embedding {embedding.shape}")
         logits = self.final_mlp(embedding[:,0,:,:])
+        print(f"Logits {logits.shape}")
         return logits
